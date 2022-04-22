e65532e86058d3599837dc884c3000c42acd66f7
Irina Shevchenko <imshev78@gmail.com>
2022-04-22 13:06:55 +0300

    Example 010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {10, 12, 31, 4, 18, 51, 16, 71, 18,};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // останавливает работу цикла при первом нахождении искомого значенияя, 
+               // в противном случае будет выдавать их все имеющиеся в массиве
+    }
+    index++;
+}
